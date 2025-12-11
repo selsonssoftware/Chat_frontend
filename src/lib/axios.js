c7ce7c7380e52a5a3fb40259d39692eabfd04ad4
selsonssoftware <165785938+selsonssoftware@users.noreply.github.com>
2025-12-11 14:39:09 +0530

    Update axios baseURL for API endpoint

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://blueviolet-grasshopper-101010.hostingersite.com" : "https://blueviolet-grasshopper-101010.hostingersite.com",
+  baseURL: import.meta.env.MODE === "development" ? "https://blueviolet-grasshopper-101010.hostingersite.com/api" : "https://blueviolet-grasshopper-101010.hostingersite.com/api",
   withCredentials: true,
 });
